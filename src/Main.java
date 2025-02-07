dfe66ae1782e0c728f62f68fa6ef4955155f5fdd
benjamin <91140626+tpdebenjamin@users.noreply.github.com>
2025-02-07 15:35:14 +0100

    Update and rename test to src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans l'application des objectifs mensuels !");
+        System.out.println("----------------------------------------------------");
+
+        // Affichage des objectifs de chaque mois
+        objectifJanvier();
+        objectifFévrier();
+        objectifMars();
+        objectifAvril();
+        objectifMai();
+        objectifJuin();
+        objectifJuillet();
+        objectifAoût();
+        objectifSeptembre();
+        objectifOctobre();
+        objectifNovembre();
+        objectifDécembre();
+    }
+
+    // Objectifs de chaque mois
+    public static void objectifJanvier() {
+        System.out.println("Janvier : Rédiger un journal intime.");
+    }
+
+    public static void objectifFévrier() {
+        System.out.println("Février : Redécorer la maison pour se sentir bien.");
+    }
+
+    public static void objectifMars() {
+        System.out.println("Mars : Prendre soin de soi.");
+    }
+
+    public static void objectifAvril() {
+        System.out.println("Avril : Soyez gentil.");
+    }
+
+    public static void objectifMai() {
+        System.out.println("Mai : Faites quelque chose que vous n’avez jamais fait auparavant.");
+    }
+
+    public static void objectifJuin() {
+        System.out.println("Juin : Passez du temps avec le vôtre.");
+    }
+
+    public static void objectifJuillet() {
+        System.out.println("Juillet : Aller à un concert et danser.");
+    }
+
+    public static void objectifAoût() {
+        System.out.println("Août : Dormir une nuit sur la plage.");
+    }
+
+    public static void objectifSeptembre() {
+        System.out.println("Septembre : Découvrez le monde.");
+    }
+
+    public static void objectifOctobre() {
+        System.out.println("Octobre : Étudiez.");
+    }
+
+    public static void objectifNovembre() {
+        System.out.println("Novembre : Apprendre à jouer d'un instrument.");
+    }
+
+    public static void objectifDécembre() {
+        System.out.println("Décembre : Respirez consciencieusement.");
+    }
+}
+
